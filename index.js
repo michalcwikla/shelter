67bc0979503d6c8ff9117dcc3c729f08083ebbfa
michal <michalcwikla95@gmail.com>
2022-03-20 23:40:40 +0100

    New version .1

---
+++ index.js
@@ -0,0 +1,13 @@
+// const demo = document.getElementById("demo");
+
+
+// document.getElementById('demo').onclick = function changeContent() {
+
+//     document.getElementById('demo').textContent = "Help me";
+//     document.getElementById('demo').style = "Color: red";
+//  }
+
+ const a = 6;
+ string b = "3";
+ let c = a+b;
+ console.log(c);
