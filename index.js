a75fd51eb65612efa3c1eca47d04cbbcff131720
Michal <michalcwikla95@gmail.com>
2022-08-13 01:50:15 +0200

    nowa wersja10

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// // const buttonColor = document.querySelector('hide');
-console.log("marcin")
+// // // const buttonColor = document.querySelector('hide');
+// console.log("marcin")
 
-naKlikniecie = () => {
-    document.getElementById('hide').style.background="green";
-    console.log("marcin1");
-}
-const buttonColor = document.querySelector('hide');
-console.log("marcin")
+// naKlikniecie = () => {
+//     document.getElementById('hide').style.background="green";
+//     console.log("marcin1");
+// }
+// const buttonColor = document.querySelector('hide');
+// console.log("marcin")
 
-naKlikniecie = () => {
-    document.querySelector.onclick(buttonColor);
+// naKlikniecie = () => {
+//     document.querySelector.onclick(buttonColor);
     
 
-}
+// }
