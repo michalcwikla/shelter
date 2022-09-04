93c0147e195ac8bd87972e41dbfe162aa3e28dcc
Michal <michalcwikla95@gmail.com>
2022-09-04 20:35:39 +0200

    nowawersja13

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const menuSlide = () => {
 
     const burger = document.querySelector(".burger");
     console.log({burger});
-    const menu1 = document.getElementsByClassName("menu");
+    const menu1 = document.querySelector(".menu");
     console.log({menu1});
 
     burger.addEventListener("click", () => {
