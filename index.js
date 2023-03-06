455341b4d549ab3e4ba59044c67e9694627a91e5
Michal <michalcwikla95@gmail.com>
2023-03-06 23:07:43 +0100

    zeby trochę ładniej wygladała

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const menuSlide = () => {
-    console.log("dzialam")
+menuSlide();
+naKlikniecie();
+onScroll();
 
+
+
+const menuSlide = () => {
+    console.log("dzialam");
     const burger = document.querySelector(".burger");
     console.log({burger});
     const menu1 = document.querySelector(".menu");
@@ -11,4 +16,27 @@ const menuSlide = () => {
     });
 }
 
-menuSlide();
+
+const naKlikniecie = () => {
+    return console.log("dzialam ja jebeeeee") ;
+    const burger = document.querySelector(".hide");
+
+
+    // burger.addEventListener("click", () => {
+    //     menu1.classList.toggle("menu-active");
+    // });
+}
+
+
+const scrollPos1 = window.pageYOffset;
+const onScroll = () => {
+const scrollPos2 = window.pageYOffset;
+  if (scrollPos1 > scrollPos2) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-50px";
+  }
+  scrollPos1 = scrollPos2;
+}
+
+
