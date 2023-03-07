85cc07c3e5e14a6d6db56e91fb229462097e88f1
Michal <michalcwikla95@gmail.com>
2023-03-07 19:06:03 +0100

    I have changed images and responses

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-menuSlide();
-naKlikniecie();
-onScroll();
-
-
 
 const menuSlide = () => {
-    console.log("dzialam");
-    const burger = document.querySelector(".burger");
-    console.log({burger});
-    const menu1 = document.querySelector(".menu");
-    console.log({menu1});
-
-    burger.addEventListener("click", () => {
-        menu1.classList.toggle("menu-active");
-    });
+  // return console.log("dzialam");
+  const burger = document.querySelector(".burger");
+  // console.log({burger});
+  const menu1 = document.querySelector(".menu");
+  // console.log({menu1});
+  burger.addEventListener("click", () => {
+    menu1.classList.toggle("menu-active");
+  });
 }
+
+menuSlide();
 
 
 const naKlikniecie = () => {
-    return console.log("dzialam ja jebeeeee") ;
+    // return console.log("dzialam ja jebeeeee") ;
     const burger = document.querySelector(".hide");
+    burger.addEventListener("click", () => {
+          menu1.classList.toggle("menu-active");
+      });
+    }
 
 
-    // burger.addEventListener("click", () => {
-    //     menu1.classList.toggle("menu-active");
-    // });
+    naKlikniecie();
+
+const menuHide = () => {
+  window.addEventListener('scroll', () => {
+    console.log('scrolled');
+    const menu2 = document.querySelector(".menu");
+  })
 }
 
-
-const scrollPos1 = window.pageYOffset;
-const onScroll = () => {
-const scrollPos2 = window.pageYOffset;
-  if (scrollPos1 > scrollPos2) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-50px";
-  }
-  scrollPos1 = scrollPos2;
-}
-
-
+menuHide();
